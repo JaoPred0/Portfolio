11d7c8f8cf6bc2c896c30b03d0242f0eeb284808
JaoPred0 <jhonpitere163@gmail.com>
2025-11-18 20:23:15 -0400

    Erro desgramado, Navbar com n minusculo

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import FloatingLines from "./components/bits/FloatingLines";
-import Navbar from "./components/navbar";
+import Navbar from "./components/Navbar";
 import HeroSection from "./components/HeroSection";
 
 const App = () => {
